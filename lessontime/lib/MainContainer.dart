59ce89c785b2ff16328354d96c12e8e04c5e8d49
BuildTools <unconfigured@null.spigotmc.org>
2018-05-29 01:46:47 +0800

    complete navbar

--- lessontime/lib/MainContainer.dart
+++ lessontime/lib/MainContainer.dart
@@ -1,16 +1,65 @@
 import "package:flutter/material.dart";
 import "NavBar.dart";
 
-class MainContainer extends StatelessWidget{
-    Container content;
-    Widget build(BuildContext Context){
-      return new Scaffold(
-        body: content,
-        bottomNavigationBar: NavBar(),
-      );
-    }
+class MainContainer extends StatefulWidget {
+	@override
+	_MainContainerState createState() => new _MainContainerState();
+}
 
-    void ChangePage(int i){
+class _MainContainerState extends State<MainContainer>
+		with SingleTickerProviderStateMixin{
 
-    }
+	TabController tabController;
+
+	@override
+  void initState() {
+		// TODO: implement initState
+		super.initState();
+		tabController = new TabController(length: 3, vsync: this);
+	}
+
+	@override
+	void dispose(){
+		tabController.dispose();
+		super.dispose();
+	}
+
+	@override
+		Widget build(BuildContext context){
+			return new Scaffold(
+				appBar: new AppBar(title:  new Text("LessonTime",textAlign: TextAlign.center,),),
+				body: new TabBarView(
+          children: <Widget>[new NewPage("First"),new NewPage("Second"),new NewPage("Third")],
+          controller: tabController,
+        ),
+				bottomNavigationBar: new Material(
+					child: new TabBar(
+						controller: tabController,
+						tabs: <Widget>[
+						new Tab(
+							icon: new Icon(Icons.home, color: Colors.red,),
+						),
+						new Tab(
+							icon: new Icon(Icons.camera, color: Colors.red,),
+						),
+						new Tab(
+							icon: new Icon(Icons.person, color: Colors.red,),
+						)
+					],)
+				),
+			);
+		}
+}
+
+class NewPage extends StatelessWidget{
+	final String title;
+	NewPage(this.title);
+	@override
+	Widget build(BuildContext context){
+		return new Scaffold(
+			body: new Center(
+				child: new Text(title),
+			),
+		);
+	}
 }
--- lessontime/lib/NavBar.dart
+++ lessontime/lib/NavBar.dart
@@ -6,42 +6,12 @@ class NavBar extends StatefulWidget {
   _NavBarState createState() => _NavBarState();
 }
 
-class _NavBarState extends State<NavBar>{
-  Widget build(BuildContext Context){
+class _NavBarState extends State<NavBar> {
+  @override
+  Widget build(BuildContext Context) {
     return new BottomNavigationBar(
-        items: _NavBarItems(),
+      items: null,
 
     );
   }
-
-  void ChangePage(int i){
-    MainContainer().ChangePage(i);
-  }
-}
-
-List<BottomNavigationBarItem> _NavBarItems(){
-   List<BottomNavigationBarItem> _navBar = new List();
-   BottomNavigationBarItem homeBtn = new BottomNavigationBarItem(
-     icon: new Icon(Icons.home),
-     title: new Text("Home"),
-
-   );
-   BottomNavigationBarItem scan = new BottomNavigationBarItem(
-     icon: new Icon(Icons.camera),
-     title: new Text("Home"),
-   );
-   BottomNavigationBarItem profile = new BottomNavigationBarItem(
-     icon: new Icon(Icons.person),
-     title: new Text("Profile"),
-
-   );
-   _navBar.add(homeBtn);
-   _navBar.add(scan);
-   _navBar.add(profile);
-
-   return _navBar;
-}
-
-void SetActive(int index){
-
 }
